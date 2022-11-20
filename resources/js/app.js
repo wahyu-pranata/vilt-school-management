5dc08806b5cd16614437cb05c60862e7c924b8ba
Wahyu Pranata <wahyupranata125@gmail.com>
2022-11-20 13:29:03 +0800

    add inertia progress indicator

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 import {createApp, h} from "vue";
 import {createInertiaApp} from "@inertiajs/inertia-vue3";
+import {InertiaProgress} from "@inertiajs/progress";
+
+InertiaProgress.init()
 
 createInertiaApp({
     resolve: name => require(`./Pages/${name}`),
