7963664ba508f8426c40ad389e0f58f1ebae7ef2
Wahyu Pranata <wahyupranata125@gmail.com>
2022-11-20 13:42:30 +0800

    add new pages

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <Head title="Home Page"></Head>
+    <Layout>
+        <h1 class="text-2xl text-sky-700">Hello, this is home page!</h1>
+    </Layout>
+
+</template>
+
+<script setup>
+import { Head } from '@inertiajs/inertia-vue3'
+import Layout from './Layout/Layout.vue'
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Layout/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+    <main>
+        <header class="flex w-100 justify-center space-x-4 bg-sky-600 py-3">
+            <Link href="/" class="text-white opacity-80 hover:opacity-100">Home</Link>
+            <Link href="/about" class="text-white opacity-80 hover:opacity-100">About</Link>
+            <Link href="/login" class="text-white opacity-80 hover:opacity-100">Login</Link>
+        </header>
+        <main class="px-4 py-2">
+            <slot/>
+        </main>
+    </main>
+</template>
+
+<script setup>
+import {Link} from '@inertiajs/inertia-vue3'
+</script>
