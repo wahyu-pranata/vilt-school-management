7dda73eb2da22aceed83c0bb683258031a2c5668
Wahyu Pranata <wahyupranata125@gmail.com>
2022-11-20 13:41:42 +0800

    fix error where require is not defined

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,7 @@ import {InertiaProgress} from "@inertiajs/progress";
 InertiaProgress.init()
 
 createInertiaApp({
-    resolve: name => require(`./Pages/${name}`),
+    resolve: name => import(`./Pages/${name}.vue`),
     setup({el, App, props, plugin}) {
         createApp({render: () => h(App, props)})
             .use(plugin)
