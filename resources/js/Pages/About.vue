27c9de53040fb0b67013413d7df66754b955d43a
Wahyu Pranata <wahyupranata125@gmail.com>
2022-11-21 13:32:09 +0800

    add about page

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,11 @@
+<template>
+    <Head title="About Page"></Head>
+    <Layout>
+        <h1 class="text-2xl text-sky-700">About this website</h1>
+    </Layout>
+</template>
+
+<script setup>
+import {Head} from "@inertiajs/inertia-vue3"
+import Layout from "./Layout/Layout.vue"
+</script>
