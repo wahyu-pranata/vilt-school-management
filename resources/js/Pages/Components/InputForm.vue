959b035fa8cca3833b2ec6f51a6d4022a81ff00d
Wahyu Pranata <wahyupranata131@gmail.com>
2022-11-21 18:30:03 +0800

    add input-form component

---
+++ resources/js/Pages/Components/InputForm.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="mb-3">
+        <label :for="id" class="text-left">{{ labelText }}</label>
+        <input :type="type" :id="id" :placeholder="placeholder">
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(['id', 'type', 'placeholder', 'labelText'])
+</script>
+
+<style scoped>
+label {
+    @apply after:content-['*'] after:ml-0.5 after:text-rose-600 after:text-xs
+}
+input {
+    @apply block w-full mt-2 px-3 py-2 font-normal font-light text-sm border border-solid border-slate-600 rounded transition focus:outline-none focus:border-sky-700 focus:ring focus:ring-blue-500
+}
+</style>
