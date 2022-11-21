8009522bc7183dcae0e1b2a41eaea00942669d37
Wahyu Pranata <wahyupranata131@gmail.com>
2022-11-21 15:10:36 +0800

    add noto sans font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                noto: ['Noto Sans', 'sans-serif']
+            }
+        },
     },
     plugins: [],
 }
