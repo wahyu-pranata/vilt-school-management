e0d86e5bdf36d1fb2bb671e008a4f30d4e4f1a06
Wahyu Pranata <wahyupranata131@gmail.com>
2022-11-25 19:14:37 +0800

    only show dashboard if the user is admin

--- resources/js/Pages/Components/Navbar.vue
+++ resources/js/Pages/Components/Navbar.vue
@@ -4,9 +4,9 @@
             <h1 class="text-white text-2xl font-light"><span class="font-bold uppercase">VILT</span> Manager</h1>
         </div>
         <div>
-            <p class="" v-if="!$page.props.auth.user"><Link href="/login" class="inline-block px-4 py-1 text-white border border-solid border-white rounded-md opacity-80 hover:opacity-100 hover:bg-white hover:text-slate-800 transition-all">Login</Link></p>
+            <p v-if="!$page.props.auth.user"><Link href="/login" class="inline-block px-4 py-1 text-white border border-solid border-white rounded-md opacity-80 hover:opacity-100 hover:bg-white hover:text-slate-800 transition-all">Login</Link></p>
             <div class="flex space-x-2" v-else>
-                <Link href="/dashboard" class="px-4 py-1 text-white opacity-80 hover:opacity-100 transition-all">Dashboard</Link>
+                <Link href="/dashboard" class="px-4 py-1 text-white opacity-80 hover:opacity-100 transition-all" v-if="$page.props.auth.user.is_admin === 1">Dashboard</Link>
                 <form @submit.prevent="Inertia.post('/logout')">
                     <button type="submit" class="px-4 py-1 text-white border border-solid border-white rounded-md opacity-80 hover:opacity-100 hover:bg-white hover:text-slate-800 transition-all">Logout</button>
                 </form>
