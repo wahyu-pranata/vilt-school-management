695a609e33e43630b8dd953c7349a633428d36f7
Wahyu Pranata <wahyupranata131@gmail.com>
2022-11-25 13:02:09 +0800

    add new dashboard page

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,9 @@
+<template>
+    <Head title="Dashboard"/>
+    <Layout></Layout>
+</template>
+
+<script setup>
+import Layout from "./Layout/Layout.vue"
+import { Head } from "@inertiajs/inertia-vue3";
+</script>
