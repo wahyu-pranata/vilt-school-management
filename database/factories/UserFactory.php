27b2994b95f67e12584b686bb4a3cd1677b4a297
Wahyu Pranata <wahyupranata131@gmail.com>
2022-11-25 13:01:55 +0800

    add admin user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -18,10 +19,12 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'email' => 'admin@example.com',
+            'first_name' => 'Vilt',
+            'last_name' => 'Admin',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('viltadmin'),
+            'is_admin' => 1,
             'remember_token' => Str::random(10),
         ];
     }
