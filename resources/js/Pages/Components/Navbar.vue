2e577b7eda77501ab277b88f4e8a2ad51e4722e4
Wahyu Pranata <wahyupranata131@gmail.com>
2022-11-25 12:02:21 +0800

    remake navbar

---
+++ resources/js/Pages/Components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav class="w-100 bg-slate-800 px-8 py-3 flex justify-between align-middle">
+        <div>
+            <h1 class="text-white text-2xl font-light"><span class="font-bold uppercase">VILT</span> Manager</h1>
+        </div>
+        <div>
+            <p class="" v-if="!$page.props.auth.user"><Link href="/login" class="inline-block px-4 py-1 text-white border border-solid border-white rounded-md opacity-80 hover:opacity-100 hover:bg-white hover:text-slate-800 transition-all">Login</Link></p>
+            <div class="flex space-x-2" v-else>
+                <Link href="/dashboard" class="px-4 py-1 text-white opacity-80 hover:opacity-100 transition-all">Dashboard</Link>
+                <form @submit.prevent="Inertia.post('/logout')">
+                    <button type="submit" class="px-4 py-1 text-white border border-solid border-white rounded-md opacity-80 hover:opacity-100 hover:bg-white hover:text-slate-800 transition-all">Logout</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3"
+import { Inertia } from "@inertiajs/inertia"
+</script>
--- resources/js/Pages/Layout/Layout.vue
+++ resources/js/Pages/Layout/Layout.vue
@@ -1,26 +1,14 @@
 <template>
-    <main>
-        <nav class="w-100 bg-slate-800 py-3">
-            <ul class="flex justify-center space-x-4">
-                <li><Link href="/" class="text-white opacity-80 hover:opacity-100">Home</Link></li>
-                <li><Link href="/about" class="text-white opacity-80 hover:opacity-100">About</Link></li>
-                <li v-if="!$page.props.auth.user"><Link href="/login" class="text-white opacity-80 hover:opacity-100">Login</Link></li>
-                <li v-else>
-                    <form @submit.prevent="Inertia.post('/logout')">
-                        <button type="submit" class="text-white opacity-80 hover:opacity-100">Logout</button>
-                    </form>
-                </li>
-            </ul>
-        </nav>
+    <div>
+        <Navbar/>
         <main>
             <slot/>
         </main>
-    </main>
+    </div>
 </template>
 
 <script setup>
-import {Link} from '@inertiajs/inertia-vue3'
-import { Inertia } from '@inertiajs/inertia'
+import Navbar from "../Components/Navbar.vue"
 
 
 </script>
