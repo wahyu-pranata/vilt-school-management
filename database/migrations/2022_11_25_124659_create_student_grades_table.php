1e8cba17a87582ab8f6bb17090b89bb2dbaea121
Wahyu Pranata <wahyupranata131@gmail.com>
2022-11-25 21:03:45 +0800

    add migration for respective models

---
+++ database/migrations/2022_11_25_124659_create_student_grades_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_grades', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\Student::class);
+            $table->foreignIdFor(\App\Models\Course::class);
+            $table->foreignIdFor(\App\Models\SchoolYear::class);
+            $table->integer('grade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_grades');
+    }
+};
