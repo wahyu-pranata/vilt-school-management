c9596a2ae4ea047380629ca1bbbbade7f41b8186
Wahyu Pranata <wahyupranata131@gmail.com>
2022-11-26 20:01:03 +0800

    add url in shared data

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -38,6 +38,7 @@ class HandleInertiaRequests extends Middleware
     {
         return array_merge(parent::share($request), [
             'auth.user' => fn () => $request->user() ? $request->user()->only('email', 'first_name', 'last_name', 'is_admin') : null,
+            'url' => fn () => $request->path(),
             'flash' => [
                 'message' => fn () => $request->session()->get('message')
             ]
--- resources/js/Pages/Components/Navbar.vue
+++ resources/js/Pages/Components/Navbar.vue
@@ -1,21 +1,33 @@
 <template>
-    <nav class="w-100 bg-slate-800 px-8 py-3 flex justify-between align-middle">
-        <div>
-            <h1 class="text-white text-2xl font-light"><span class="font-bold uppercase">VILT</span> Manager</h1>
-        </div>
-        <div>
-            <p v-if="!$page.props.auth.user"><Link href="/login" class="inline-block px-4 py-1 text-white border border-solid border-white rounded-md opacity-80 hover:opacity-100 hover:bg-white hover:text-slate-800 transition-all">Login</Link></p>
-            <div class="flex space-x-2" v-else>
-                <Link href="/dashboard" class="px-4 py-1 text-white opacity-80 hover:opacity-100 transition-all" v-if="$page.props.auth.user.is_admin === 1">Dashboard</Link>
-                <form @submit.prevent="Inertia.post('/logout')">
-                    <button type="submit" class="px-4 py-1 text-white border border-solid border-white rounded-md opacity-80 hover:opacity-100 hover:bg-white hover:text-slate-800 transition-all">Logout</button>
-                </form>
+    <header class="w-100">
+        <nav class="bg-slate-800 px-8 py-3 flex justify-between align-middle">
+            <div>
+                <h1 class="text-white text-2xl font-light"><Link href="/"><span class="font-bold uppercase">VILT</span> Manager</Link></h1>
             </div>
-        </div>
-    </nav>
+            <div>
+                <p v-if="!$page.props.auth.user"><Link href="/login" class="inline-block px-4 py-1 text-white border border-solid border-white rounded-md opacity-80 hover:opacity-100 hover:bg-white hover:text-slate-800 transition-all">Login</Link></p>
+                <div class="flex space-x-2" v-else>
+                    <Link href="/dashboard" class="px-4 py-1 text-white/80 hover:text-white transition-all" v-if="$page.props.auth.user.is_admin === 1">Dashboard</Link>
+                    <form @submit.prevent="Inertia.post('/logout')">
+                        <button type="submit" class="px-4 py-1 text-white border border-solid border-white rounded-md opacity-80 hover:opacity-100 hover:bg-white hover:text-slate-800 transition-all">Logout</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <nav class="bg-slate-700 text-white px-8 py-1" v-if="$page.props.url.includes('dashboard')">
+            <ul class="flex space-x-4">
+                <li><Link href="/dashboard/student" class="text-white/80 hover:text-white font-light">Student</Link></li>
+                <li><Link href="#" class="text-white/80 hover:text-white font-light">Class</Link></li>
+                <li><Link href="#" class="text-white/80 hover:text-white font-light">Teacher</Link></li>
+                <li><Link href="#" class="text-white/80 hover:text-white font-light">Course</Link></li>
+                <li><Link href="#" class="text-white/80 hover:text-white font-light">Grade</Link></li>
+            </ul>
+        </nav>
+    </header>
 </template>
 
 <script setup>
 import { Link } from "@inertiajs/inertia-vue3"
 import { Inertia } from "@inertiajs/inertia"
+
 </script>
