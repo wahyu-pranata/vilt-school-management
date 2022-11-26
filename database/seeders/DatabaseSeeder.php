fbd0c4899bd9522ebaf3bab2b7dd5515ef094b6f
Wahyu Pranata <wahyupranata131@gmail.com>
2022-11-26 19:59:45 +0800

    add factory for student

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\Student::factory(25)->create();
 
-         \App\Models\User::factory()->create();
+//         \App\Models\User::factory()->create();
     }
 }
