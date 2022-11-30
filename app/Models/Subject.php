8d3a2921a3de843b95558c418a3ca967727087ec
Wahyu Pranata <wahyupranata131@gmail.com>
2022-11-30 19:57:33 +0800

    add relationship in models

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Subject extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    public function courses() {
+        return $this->hasMany(Course::class);
+    }
+
+    public function teachers() {
+        return $this->hasMany(Teacher::class);
+    }
 }
