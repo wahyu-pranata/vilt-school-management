13436155b378ef2ca5f476968562c55f50b39a67
Wahyu Pranata <wahyupranata131@gmail.com>
2022-11-30 11:01:46 +0800

    add factories for existing models

---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course>
+ */
+class CourseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'subject_id' => fake()->numberBetween(1, 5),
+            'course_name' => fake()->jobTitle()
+        ];
+    }
+}
